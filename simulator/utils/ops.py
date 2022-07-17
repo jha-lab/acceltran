2b2c28e38b0abb17d06f76afb47afd109d3c7bcf
shikhartuli <shikhartuli98@gmail.com>
2022-07-16 20:03:45 -0400

    Updated operations

--- simulator/utils/ops.py
+++ simulator/utils/ops.py
@@ -1,12 +1,15 @@
 # Software operations in the Transformer architecture
 
+from tiled_ops import *
+
 
 # TODO: add tiling and module assignment
 
 class Op(object):
 	"""Class for a generic Transformer operation"""
-	def __init__(self, op_name):
+	def __init__(self, op_name, config):
 		self.name = op_name
+		self.config = config
 		self.done = False
 
 
@@ -14,10 +17,12 @@ class MemoryLoadOp(Op):
 	"""Memory load operation
 	
 	Attributes:
+		input_size (tuple): size of the input matrix to be loaded
 		data_type (str): type of data to fetch in ['activation', 'weight']
 	"""
-	def __init__(self, op_name, data_type):
-		Op.__init__(self, op_name)
+	def __init__(self, op_name, config, input_size, data_type):
+		Op.__init__(self, op_name, config)
+		self.input_size = input_size
 		self.data_type = data_type
 
 
@@ -29,8 +34,8 @@ class SelfAttentionOp(Op):
 		hidden_size (int): hidden size of the attention head
 		type (str): type of self-attention in ['sdp', 'wma']
 	"""
-	def __init__(self, op_name, input_size, hidden_size, type):
-		Op.__init__(self, op_name)
+	def __init__(self, op_name, config, input_size, hidden_size, type):
+		Op.__init__(self, op_name, config)
 		self.input_size = input_size
 		self.hidden_size = hidden_size
 		self.type = type
@@ -44,8 +49,8 @@ class ConvOp(Op):
 		hidden_size (int): hidden size of the attention head
 		kernel_size (int): size of the convolutional kernel
 	"""
-	def __init__(self, op_name, input_size, hidden_size, kernel_size):
-		Op.__init__(self, op_name)
+	def __init__(self, op_name, config, input_size, hidden_size, kernel_size):
+		Op.__init__(self, op_name, config)
 		self.input_size = input_size
 		self.hidden_size = hidden_size
 		self.kernel_size = kernel_size
@@ -59,8 +64,8 @@ class LinearTransformOp(Op):
 		hidden_size (int): hidden size of the attention head
 		type (scr): type of linear transformation in ['dct', 'dft']
 	"""
-	def __init__(self, op_name, input_size, hidden_size, type):
-		Op.__init__(self, op_name)
+	def __init__(self, op_name, config, input_size, hidden_size, type):
+		Op.__init__(self, op_name, config)
 		self.input_size = input_size
 		self.hidden_size = hidden_size
 		self.type = type
@@ -73,8 +78,8 @@ class FeedForwardOp(Op):
 		input_size (tuple): size of the input matrix
 		hidden_size (int): hidden size of the attention head
 	"""
-	def __init__(self, op_name, input_size, hidden_size):
-		Op.__init__(self, op_name)
+	def __init__(self, op_name, config, input_size, hidden_size):
+		Op.__init__(self, op_name, config)
 		self.input_size = input_size
 		self.hidden_size = hidden_size
 
@@ -85,8 +90,8 @@ class LayerNormOp(Op):
 	Attributes:
 		input_size (tuple): size of the input matrix
 	"""
-	def __init__(self, op_name, input_size):
-		Op.__init__(self, op_name)
+	def __init__(self, op_name, config, input_size):
+		Op.__init__(self, op_name, config)
 		self.input_size = input_size
 
 
@@ -97,8 +102,8 @@ class NonLinearityOp(Op):
 		input_size (tuple): size of the input matrix
 		type (str): type of non-linearity in ['relu', 'gelu']
 	"""
-	def __init__(self, op_name, input_size, type):
-		Op.__init__(self, op_name)
+	def __init__(self, op_name, config, input_size, type):
+		Op.__init__(self, op_name, config)
 		self.input_size = input_size
 		self.type = type
 
@@ -108,7 +113,7 @@ class SoftmaxOp(Op):
 	Attributes:
 		input_size (tuple): size of the input matrix
 	"""
-	def __init__(self, op_name, input_size):
-		Op.__init__(self, op_name)
+	def __init__(self, op_name, config, input_size):
+		Op.__init__(self, op_name, config)
 		self.input_size = input_size
 
