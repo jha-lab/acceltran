54c33d7d7896ffc2cbdcefe9d4be3980cb4bef84
shikhartuli <shikhartuli98@gmail.com>
2022-07-19 14:58:02 -0400

    Added pe class

---
+++ simulator/utils/pe.py
@@ -0,0 +1,24 @@
+# Run tiled operations on corresponding hardware modules
+
+import math
+from ops import *
+from tiled_ops import *
+from modules import *
+
+
+class ProcessingElement(object):
+    """Processing Element class
+    """
+    def __init__(self, pe_name, config, constants):
+    	self.pe_name = pe_name
+
+    	self.mac_lanes = []
+    	for n in range(config['lanes_per_pe']):
+    		self.mac_lanes.append(MACLane(f'{self.pe_name}_maclane{(n + 1)}', config, constants))
+
+    	self.dataflow = Dataflow(f'{self.pe_name}_df', constants)
+    	self.dma = DMA(f'{self.pe_name}_dma', constants)
+
+    	self.layer_norm = LayerNorm(f'{self.pe_name}_ln', constants)
+    	self.softmax = Softmax(f'{self.pe_name}_sftm', constants)
+
