e09f19041c90228b81f9c2ce907d0056e64cb53a
shikhartuli <shikhartuli98@gmail.com>
2022-07-20 16:09:06 -0400

    Bug fixes

--- simulator/src/accelerator.py
+++ simulator/src/accelerator.py
@@ -9,21 +9,21 @@ from buffer import Buffer
 
 
 class Accelerator(object):
-    """Accelerator class
-    
-    Attributes:
-    	pes (list): list of ProcessingElement objects
-        activation_buffer (Buffer): Buffer class object for activations
-        mask_buffer (Buffer): Buffer class object for binary masks
-        weight_buffer (Buffer): Buffer class object for weights
-    """
-    def __init__(self, config, constants)
-    	self.pes = []
-   		for p in range(config['pe']):
-   			self.pes.append(ProcessingElement(f'pe{p}', config, constants))
+	"""Accelerator class
+	
+	Attributes:
+		pes (list): list of ProcessingElement objects
+		activation_buffer (Buffer): Buffer class object for activations
+		mask_buffer (Buffer): Buffer class object for binary masks
+		weight_buffer (Buffer): Buffer class object for weights
+	"""
+	def __init__(self, config, constants):
+		self.pes = []
+		for p in range(config['pe']):
+			self.pes.append(ProcessingElement(f'pe{p}', config, constants))
 
-   		self.activation_buffer = Buffer('activation', config, constants)
-   		self.weight_buffer = Buffer('weight', config, constants)
-   		self.mask_buffer = Buffer('mask', config, constants)
+		self.activation_buffer = Buffer('activation', config, constants)
+		self.weight_buffer = Buffer('weight', config, constants)
+		self.mask_buffer = Buffer('mask', config, constants)
 
-   		# TODO: add main memory object with its leakage energy
+		# TODO: add main memory object with its leakage energy
