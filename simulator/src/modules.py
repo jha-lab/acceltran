7745893ace0c8165a52115fbb51f14a979cd8380
shikhartuli <shikhartuli98@gmail.com>
2022-07-21 16:39:56 -0400

    Added cycle processing

--- simulator/src/modules.py
+++ simulator/src/modules.py
@@ -1,6 +1,7 @@
 # Run tiled operations on corresponding hardware modules
 
 import math
+import inspect
 from ops import *
 from tiled_ops import *
 
@@ -23,73 +24,110 @@ class Module(object):
 		self.process_cycles = None 
 		self.ready = True
 
+	def process_cycle(self):
+		if self.process_cycles == 0:
+			self.ready = True
+		else:
+			self.process_cycles -= 1
+			self.ready = False
+
+		for member in inspect.getmembers(self):
+			if isinstance(member, Module):
+				member.process_cycle()
+				if not member.ready: self.ready = False
+
+		if self.ready and self.assigned_op is not None:
+			self.assigned_op.done = True
+
 
 class Dataflow(Module):
 	def __init__(self, module_name, constants):
 		Module.__init__(self, module_name, constants['dataflow']['dynamic'], constants['dataflow']['leakage'], constants['dataflow']['area'])
+		self.assigned_op = None
 
-	def process(self, op):
+	def assign_op(self, op):
 		self.process_cycles = 1
 		self.ready = False
+
+		self.assigned_op = op
 
 
 class DMA(Module):
 	def __init__(self, module_name, constants):
 		Module.__init__(self, module_name, constants['dma']['dynamic'], constants['dma']['leakage'], constants['dma']['area'])
+		self.assigned_op = None
 
-	def process(self, op):
+	def assign_op(self, op):
 		self.process_cycles = 1
 		self.ready = False
+
+		self.assigned_op = op
 
 
 class LayerNorm(Module):
 	def __init__(self, module_name, constants, width=16):
 		Module.__init__(self, module_name, constants['layer_norm']['dynamic'], constants['layer_norm']['leakage'], constants['layer_norm']['area'])
 		self.width = width
+		self.assigned_op = None
 
-	def process(self, op):
+	def assign_op(self, op):
 		# TODO: check cycles based on RTL
 		self.process_cycles = math.ceil(sum(op.input_size) * 1.0 / self.width)
 		self.ready = False
+
+		self.assigned_op = op
 
 
 class Softmax(Module):
 	def __init__(self, module_name, constants, width=16):
 		Module.__init__(self, module_name, constants['softmax']['dynamic'], constants['softmax']['leakage'], constants['softmax']['area'])
 		self.width = width
+		self.assigned_op = None
 
-	def process(self, op):
+	def assign_op(self, op):
 		# TODO: check cycles based on RTL
 		self.process_cycles = math.ceil(sum(op.input_size) * 1.0 / self.width)
 		self.ready = False
+
+		self.assigned_op = op
 
 
 class Register(Module):
 	def __init__(self, module_name, constants, depth):
 		Module.__init__(self, module_name, constants['register']['dynamic'], constants['register']['leakage'], constants['register']['area'])
 		self.depth = depth
+		self.assigned_op = None
 
-	def process(self, op):
+	def assign_op(self, op):
 		self.process_cycles = 1
 		self.ready = False
+
+		self.assigned_op = op
 
 
 class PreSparsity(Module):
 	def __init__(self, module_name, constants):
 		Module.__init__(self, module_name, constants['pre_sparsity']['dynamic'], constants['pre_sparsity']['leakage'], constants['pre_sparsity']['area'])
+		self.assigned_op = None
 
-	def process(op):
+	def assign_op(op):
 		self.process_cycles = 1
 		self.ready = False
+
+		self.assigned_op = op
 
 
 class PostSparsity(Module):
 	def __init__(self, module_name, constants):
 		Module.__init__(self, module_name, constants['post_sparsity']['dynamic'], constants['post_sparsity']['leakage'], constants['post_sparsity']['area'])
+		self.assigned_op = None
 
-	def process(self, op):
+	def assign_op(self, op):
 		self.process_cycles = 1
 		self.ready = False
+
+		self.assigned_op = op
+
 
 class MACLane(Module):
 	def __init__(self, module_name, config, constants):
@@ -98,14 +136,21 @@ class MACLane(Module):
 		self.activation_sparsity = constants['sparsity']['activation']
 		self.weight_sparsity = constants['sparsity']['weight']
 		self.overlap_factor = constants['overlap_factor']
+		self.assigned_op = None
 
 		self.register = Register(f'{self.module_name}_reg', constants, 32)
 		self.pre_sparsity = PreSparsity(f'{self.module_name}_pre-s', constants)
 		self.post_sparsity = PostSparsity(f'{self.module_name}_post-s', constants)
 
-	def process(self, op):
+	def assign_op(self, op):
 		# TODO: add activation and weight sparsity
 		# TODO: add num_muls in corresponding tiled op
 		self.process_cycles = math.ceil(op.num_muls * 1.0 / self.num_macs)
 		self.ready = False
+
+		self.assigned_op = op
+
+		self.register.assign_op(op)
+		self.pre_sparsity.assign_op(op)
+		self.post_sparsity.assign_op(op)
 
