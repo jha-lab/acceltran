6ad5ae9a78a69434e9861d30d19dc2fa02daf019
shikhartuli <shikhartuli98@gmail.com>
2022-07-23 15:47:15 -0400

    Made sequential

--- synthesis/top/layer_mean.sv
+++ synthesis/top/layer_mean.sv
@@ -1,33 +1,32 @@
-module mean
+module layer_mean
+#(
+	parameter size = 16,
+	parameter IL = 4, 
+	parameter FL = 16
+)
 (
-	start,
-	inp,
-	size,
-	out
+	input start,
+	input logic signed [IL+FL-1:0] inp [size-1:0][size-1:0],
+	input clk,
+	output logic signed [IL+FL-1:0] out
 );
 
-parameter IL = 4, FL = 16;
-parameter tile = size * size;
+parameter tile = size*size;
 parameter width = $clog2(size);
-
-input start;
-
-output logic signed [IL+FL-1:0] out;
 
 logic signed [(IL+FL)*2-1:0] sum;
 integer i, j;
 
-always_comb begin
+always_ff @(posedge clk) begin
 	if (start) begin
-		sum = 0;
-	end
-	else begin
+		sum <= '{default: '0};
+	end else begin
 		for (i = 0; i < size; i = i + 1) begin
 			for (j = 0; j < size; j = j + 1) begin
-				sum = sum + inp[i][j];		
+				sum <= sum + inp[i][j];		
 			end
 		end	
-		out = sum / tile;
+		out <= sum / tile;
 	end
 end
 
