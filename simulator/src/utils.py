d7291e63ee1066ad7438ef77d5ed60e0c787edfc
shikhartuli <shikhartuli98@gmail.com>
2022-07-23 17:48:41 -0400

    Added utility functions

---
+++ simulator/src/utils.py
@@ -0,0 +1,13 @@
+# Utility functions
+
+class color:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    RED = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
